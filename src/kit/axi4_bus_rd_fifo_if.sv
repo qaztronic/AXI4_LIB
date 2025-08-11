9f4ae62f047799f28ff7d864d8a4ce8997db3cec
qaztronic <qaztronic@gmail.com>
2025-08-11 10:05:04 -0700

    -

--- src/kit/axi4_bus_rd_fifo_if.sv
+++ src/kit/axi4_bus_rd_fifo_if.sv
@@ -39,14 +39,16 @@ interface axi4_bus_rd_fifo_if #(axi4_lite_pkg::axi4_lite_cfg_t C='{default: 0})
   assign ar_flat_in = araddr;
 
   wire [AR_W-1:0] ar_flat_out;
-/* verilator lint_off UNUSEDSIGNAL */
+  /* verilator lint_off UNUSEDSIGNAL */
   wire [ C.A-1:0] _araddr = ar_flat_out;
-/* verilator lint_on UNUSEDSIGNAL */
+  /* verilator lint_on UNUSEDSIGNAL */
 
   // --------------------------------------------------------------------
+  /* verilator lint_off UNUSEDSIGNAL */
   /* verilator lint_off UNDRIVEN */
   wire [(8*C.N)-1:0] rdata;
   wire [        1:0] rresp;
+  /* verilator lint_on UNUSEDSIGNAL */
   /* verilator lint_on UNDRIVEN */
   axi4_lite_r_t r;
   wire [R_W-1:0] r_flat_in;
